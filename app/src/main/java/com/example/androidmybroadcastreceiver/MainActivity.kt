f69a7b56307bebce4170db14b3b691b7e1b5aaa0
pamungkas andono <pamungkas.org@gmail.com>
2021-03-31 13:03:47 +0700

    download broadcast done

--- app/src/main/java/com/example/androidmybroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/example/androidmybroadcastreceiver/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.androidmybroadcastreceiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.example.androidmybroadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var binding: ActivityMainBinding? = null
+    private lateinit var downloadReceiver: BroadcastReceiver
 
     companion object {
         const val ACTION_DOWNLOAD_STATUS = "download_status"
@@ -22,6 +28,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         binding?.btnPermission?.setOnClickListener(this)
         binding?.btnDownload?.setOnClickListener(this)
+
+        downloadReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.d("Debug", "Download Selesai")
+                Toast.makeText(context, "Download Selesai", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val downloadIntentFilter = IntentFilter(ACTION_DOWNLOAD_STATUS)
+        registerReceiver(downloadReceiver, downloadIntentFilter)
     }
 
     override fun onClick(v: View?) {
@@ -34,7 +49,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 )
             }
             R.id.btn_download -> {
-                
+                val downloadServiceIntent = Intent(this, DownloadService::class.java)
+                startService(downloadServiceIntent)
             }
         }
     }
@@ -58,6 +74,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onDestroy() {
         super.onDestroy()
+        unregisterReceiver(downloadReceiver)
         binding = null
     }
 }
